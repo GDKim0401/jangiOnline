754d51314c5aded1c804bffde8400a297b517e13
kimsintae <kimsintae0401@gamil.com>
2024-01-28 17:41:09 +0900

    유닛 이미지작업중

--- js/mal_module.js
+++ js/mal_module.js
@@ -40,7 +40,69 @@ class JANG{
     }
 }
 
+class CHA_LEFT{
+    constructor(team){
+        this.name = 'CHA';
+        this.selected = false;
+        this.team = team;
+        this.team=='blue'?this.init_position = [0,0]:this.init_position = [0,880];
+        this.current = '';//현재위치
+        this.prev = '';//이전위치
+        this.width = 50;
+        this.height = 50;
+        this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+    
+    setUnit(){
+        this.current = this.init_position;
+        let unitDiv = document.createElement('div');
+        unitDiv.dataset.team = this.team;
+        unitDiv.dataset.name = this.name;
+        unitDiv.id = this.team+'-'+this.name;
+        unitDiv.className = 'units'
+        unitDiv.style.position = 'absolute';
+        unitDiv.style.background = this.color;
+        unitDiv.style.width = this.width+'px';
+        unitDiv.style.height = this.height+'px';
+        unitDiv.style.top = (this.init_position[1]-(this.height/2))+'px';
+        unitDiv.style.left = (this.init_position[0]-(this.width/2))+'px';
+        unitDiv.style.borderRadius = '50px';
+        document.getElementById('content').appendChild(unitDiv);
+        UNITS[this.team][this.name] = this;
+    }
+}
 
+class CHA_RIGHT{
+    constructor(team){
+        this.name = 'CHA';
+        this.selected = false;
+        this.team = team;
+        this.team=='blue'?this.init_position = [800,0]:this.init_position = [800,880];
+        this.current = '';//현재위치
+        this.prev = '';//이전위치
+        this.width = 50;
+        this.height = 50;
+        this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+    
+    setUnit(){
+        this.current = this.init_position;
+        let unitDiv = document.createElement('div');
+        unitDiv.dataset.team = this.team;
+        unitDiv.dataset.name = this.name;
+        unitDiv.id = this.team+'-'+this.name;
+        unitDiv.className = 'units'
+        unitDiv.style.position = 'absolute';
+        unitDiv.style.background = this.color;
+        unitDiv.style.width = this.width+'px';
+        unitDiv.style.height = this.height+'px';
+        unitDiv.style.top = (this.init_position[1]-(this.height/2))+'px';
+        unitDiv.style.left = (this.init_position[0]-(this.width/2))+'px';
+        unitDiv.style.borderRadius = '50px';
+        document.getElementById('content').appendChild(unitDiv);
+        UNITS[this.team][this.name] = this;
+    }
+}
 /*
     유닛 셋팅 함수
 */
