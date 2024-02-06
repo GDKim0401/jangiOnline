d25c215c734887064cc3e66ad9a71704f657a828
kimsintae <kimstcool01@gmail.com>
2024-02-06 23:22:14 +0900

    GD

--- js/mal_module.js
+++ js/mal_module.js
@@ -10,12 +10,13 @@ let UNITS = {
 */
 class JANG{
     constructor(team){
-        this.name = 'JANG';
         this.selected = false;
+        this.name = 'JANG';
         this.team = team;
         this.team=='blue'?this.init_position = [400,110]:this.init_position = [400,770];
         this.current = '';//현재위치
         this.prev = '';//이전위치
+        this.posibleMove='';
         this.width = 70;
         this.height = 70;
         this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
@@ -24,12 +25,14 @@ class JANG{
     setUnit(){
         this.current = this.init_position;
         let unitDiv = document.createElement('div');
+        unitDiv.style.background = "url('img/"+this.team+"_"+this.name+".PNG') no-repeat"; 
+        unitDiv.style.backgroundSize = "cover";
         unitDiv.dataset.team = this.team;
         unitDiv.dataset.name = this.name;
         unitDiv.id = this.team+'-'+this.name;
         unitDiv.className = 'units'
         unitDiv.style.position = 'absolute';
-        unitDiv.style.background = this.color;
+        //unitDiv.style.background = this.color;
         unitDiv.style.width = this.width+'px';
         unitDiv.style.height = this.height+'px';
         unitDiv.style.top = (this.init_position[1]-(this.height/2))+'px';
@@ -133,12 +136,12 @@ const selected = (_unit,_tag) =>{
     if (_unit.selected) {
         //이동취소 or 완료 유닛
         _unit.selected = false;
-        _tag.style.background = _unit.color;
+        _tag.style.border = 'none';
         SELECTED = [];
     }else{
         //이동준비중인 유닛
         _unit.selected = true;
-        _tag.style.background = 'rgba(0,255,0,0.6)';
+        _tag.style.border = '3px solid #424242';
         SELECTED.push({unit : _unit, tag : _tag});
     }
 }
