932e6f618d27fb52db4066ea13691f16389d1b9a
kimsintae <kimstcool01@gmail.com>
2024-02-20 22:53:53 +0900

    커밋

--- js/mal_module.js
+++ js/mal_module.js
@@ -1,218 +1,52 @@
+let bgm = new Audio();
 let SELECTED = [];
 let UNITS = {
     blue:{},
     red:{}
 };
-
+let SOUNDS = {
+    bgm : {
+        src:"./sound/TerranBGM.mp3"
+    },
+    move : {
+        src:"./sound/move.mp3"
+    },
+    attack : {
+        src:"./sound/unitclick.mp3"
+    },
+    click: {
+        src:"./sound/click.mp3"
+    },
+    play:function(type,audio){
+        audio.src = this[type].src;
+        console.log(audio)
+        audio.play();
+    }
+}
 
 /*
-    유닛 객체
+    게임 시작
 */
-class JANG{
-    constructor(){
-        this.name = 'JANG';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 80;
-        this.height = 80;
-        this.color;
-    }
-    
-    setUnit(team){
-        this.team = team;
-        if(team === 'blue'){this.init_position = [400,110];}
-        else if(team === 'red'){this.init_position = [400,770];}
-
-        this.current = this.init_position;
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
+const start=()=>{   
+    SOUNDS.play('bgm',bgm);
+    init();
 }
 
-
-class CHA{
-    constructor(){
-        this.name = 'CHA';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 60;
-        this.height = 60;
-        this.color;
-    }
-    
-    setUnit(team,up){
-        this.team = team;
-        if(team === 'blue' && up === 'L'){this.init_position = [0,0];}
-        else if(team === 'blue' && up === 'R'){this.init_position = [800,0];}
-        else if(team === 'red' && up === 'L'){this.init_position = [0,880];}
-        else if(team === 'red' && up === 'R'){this.init_position = [800,880];}
-
-        this.current = this.init_position;
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
+/*
+    게임 리셋
+*/
+const reset=()=>{
+    SELECTED = [];
+    UNITS = {
+        blue:{},
+        red:{}
+    };
+    SOUNDS.play('bgm',bgm);
+    removeDivByClass('units');
+    removeDivByClass('apoint');
+    init();
 }
 
-
-class PO{
-    constructor(){
-        this.name = 'PO';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 60;
-        this.height = 60;
-        this.color;
-        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
-    }
-            
-    setUnit(team,up){
-        this.team = team;
-        if(team === 'blue' && up === 'L'){this.init_position = [100,220];}
-        else if(team === 'blue' && up === 'R'){this.init_position = [700,220];}
-        else if(team === 'red' && up === 'L'){this.init_position = [100,660];}
-        else if(team === 'red' && up === 'R'){this.init_position = [700,660];}
-
-        this.current = this.init_position;
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
-}
-
-
-class JOL{
-    constructor(){
-        this.name = 'JOL';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 45;
-        this.height = 45;
-        this.color;
-        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
-    }
-            
-    setUnit(team,up){
-        this.team = team;
-        if(team === 'blue' && up === '1'){this.init_position = [0,330];}
-        else if(team === 'blue' && up === '2'){this.init_position = [200,330];}
-        else if(team === 'blue' && up === '3'){this.init_position = [400,330];}
-        else if(team === 'blue' && up === '4'){this.init_position = [600,330];}
-        else if(team === 'blue' && up === '5'){this.init_position = [800,330];}
-        else if(team === 'red' && up === '1'){this.init_position = [0,550];}
-        else if(team === 'red' && up === '2'){this.init_position = [200,550];}
-        else if(team === 'red' && up === '3'){this.init_position = [400,550];}
-        else if(team === 'red' && up === '4'){this.init_position = [600,550];}
-        else if(team === 'red' && up === '5'){this.init_position = [800,550];}
-        
-        this.current = this.init_position;
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
-}
-
-
-class MA{
-    constructor(){
-        this.name = 'MA';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 60;
-        this.height = 60;
-        this.color;
-        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
-    }
-            
-    setUnit(team,up,up2){
-        this.team = team;
-        if(team === 'blue' && up === 'L' && up2 === 'L'){this.init_position = [100,0];}
-        else if(team === 'blue' && up === 'L' && up2 === 'R'){this.init_position = [200,0];}
-        else if(team === 'blue' && up === 'R' && up2 === 'L'){this.init_position = [600,0];}
-        else if(team === 'blue' && up === 'R' && up2 === 'R'){this.init_position = [700,0];}
-        else if(team === 'red' && up === 'L' && up2 === 'L'){this.init_position = [100,880];}
-        else if(team === 'red' && up === 'L' && up2 === 'R'){this.init_position = [200,880];}
-        else if(team === 'red' && up === 'R' && up2 === 'L'){this.init_position = [600,880];}
-        else if(team === 'red' && up === 'R' && up2 === 'R'){this.init_position = [700,880];}
-        
-        this.current = this.init_position;
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
-}
-
-
-class SANG{
-    constructor(){
-        this.name = 'SANG';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 60;
-        this.height = 60;
-        this.color;
-        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
-    }
-            
-    setUnit(team,up,up2){
-        this.team = team;
-        if(team === 'blue' && up === 'L' && up2 === 'L'){this.init_position = [100,0];}
-        else if(team === 'blue' && up === 'L' && up2 === 'R'){this.init_position = [200,0];}
-        else if(team === 'blue' && up === 'R' && up2 === 'L'){this.init_position = [600,0];}
-        else if(team === 'blue' && up === 'R' && up2 === 'R'){this.init_position = [700,0];}
-        else if(team === 'red' && up === 'L' && up2 === 'L'){this.init_position = [100,880];}
-        else if(team === 'red' && up === 'L' && up2 === 'R'){this.init_position = [200,880];}
-        else if(team === 'red' && up === 'R' && up2 === 'L'){this.init_position = [600,880];}
-        else if(team === 'red' && up === 'R' && up2 === 'R'){this.init_position = [700,880];}
-        
-        this.current = this.init_position;
-        let unitDiv = document.createElement('div');
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
-}
-
-class SA{
-    constructor(){
-        this.name = 'SA';
-        this.selected = false;
-        this.team;
-        this.init_position;
-        this.current;//현재위치
-        this.prev;//이전위치
-        this.width = 45;
-        this.height = 45;
-        this.color;
-        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
-    }
-            
-    setUnit(team,up,up2){
-        this.team = team;
-        if(team === 'blue' && up === 'L'){this.init_position = [300,0];}
-        else if(team === 'blue' && up === 'R'){this.init_position = [500,0];}
-        else if(team === 'red' && up === 'L'){this.init_position = [300,880];}
-        else if(team === 'red' && up === 'R'){this.init_position = [500,880];}
-        
-        this.current = this.init_position;
-        let unitDiv = document.createElement('div');
-        document.getElementById('content').appendChild(set_style(this));
-        UNITS[this.team][this.name] = this;
-    }
-}
 
 
 /*
@@ -222,7 +56,8 @@ class SA{
 const set_style=(_this)=>{
     let unitDiv = document.createElement('div');
     unitDiv.style.background = "url('img/"+_this.team+"_"+_this.name+".PNG') no-repeat"; 
-    unitDiv.style.backgroundSize = "cover";
+    unitDiv.style.backgroundSize = "contain";
+    unitDiv.style.backgroundPosition = "center";
     unitDiv.dataset.team = _this.team;
     unitDiv.dataset.name = _this.name;
     unitDiv.id = _this.team+'-'+_this.name;
@@ -233,7 +68,7 @@ const set_style=(_this)=>{
     unitDiv.style.height = _this.height+'px';
     unitDiv.style.top = (_this.init_position[1]-(_this.height/2))+'px';
     unitDiv.style.left = (_this.init_position[0]-(_this.width/2))+'px';
-    unitDiv.style.borderRadius = '50px';
+    // unitDiv.style.borderRadius = '10px';
     return unitDiv;
 }
 
@@ -242,19 +77,51 @@ const set_style=(_this)=>{
 /*
     유닛 경로 함수
 */
+const calculMovePosition=()=>{//유닛이동범위계산
+    console.log("현재위치 >> "+getUnitPosition());
+    console.log(getUnit())
+    let currentPosition = getUnitPosition();
+    let accessZonePs=[];
+    let unit = getUnit();
+    let X_min = 0;
+    let X_max = 8;
+    let Y_min = 0;
+    let Y_max = 8;
+    let DIRECTION = getUnit().DIRECTION;
+    
+    // console.log(" ---- 이동 가능한 지역 ---- ");
+    for (let d = 0; d < DIRECTION.length; d++) {
+        //console.log(DIRECTION[d][0] + ', ' +DIRECTION[d][1])
+        let d_x = currentPosition[0]+(DIRECTION[d][0]*100);
+        let d_y = currentPosition[1]+(DIRECTION[d][1]*100);
+        //if(DIRECTION[d][0]<X_min || DIRECTION[d][0]>X_max){continue;};
+        if(currentPosition[0] == d_x && currentPosition[1] == d_y){continue;};//같은위치는 표시하지 않는다.
+        accessZonePs.push([currentPosition[0]+(DIRECTION[d][0]*100),currentPosition[1]+(DIRECTION[d][1]*100)]);
+        makeAccessPoint(currentPosition[0]+(DIRECTION[d][0]*100),currentPosition[1]+(DIRECTION[d][1]*100));
+       // console.log(currentPosition[0]+(DIRECTION[d][0]*100), ' ::' , currentPosition[1]+(DIRECTION[d][1]*100))
+    }
+    console.log(" ---- 이동 가능한 지역 >" ,accessZonePs);
+
+}
+
 
 /*
     유닛 이동 함수
 */
 const move=(_target)=>{
+    console.log(Math.floor(_target.dataset.x/100) + " / " + Math.floor(_target.dataset.y/100))
     if(SELECTED.length > 0){
         console.log("선택된 유닛 있음")
+       // SOUNDS.play('attack',bgm);
         let s_unit = SELECTED[0];
         s_unit['tag'].style.left = (_target.dataset.x-(s_unit.unit.width/2))+'px';
         s_unit['tag'].style.top = (_target.dataset.y-(s_unit.unit.height/2))+'px';
+        getUnit().current = [Number(_target.dataset.x),Number(_target.dataset.y)];
+        removeDivByClass('apoint');//이동할때마다 이동가능 포인트요소 지워주기
         selected(s_unit['unit'],s_unit['tag'])
     }
 }
+
 /*
     유닛 선택 함수
 */
@@ -269,8 +136,7 @@ const selected = (_unit,_tag) =>{
         _unit.selected = true;
         _tag.style.border = '3px solid #424242';
         SELECTED.push({unit : _unit, tag : _tag});
-        move();
-
+        calculMovePosition();
     }
 }
 
@@ -279,20 +145,34 @@ const selected = (_unit,_tag) =>{
 */
 const attach=(unit,moveTO)=>{
     
-
+}
+const getUnitPosition=()=>{
+    return [Math.floor(SELECTED[0].unit.current[0]),Math.floor(SELECTED[0].unit.current[1])];
+}
+const getUnit=()=>{
+    return SELECTED[0]['unit'];
 }
 
-
-/*
-    게임 리셋
-*/
-const reset=()=>{
-    SELECTED = [];
-    UNITS = {
-        blue:{},
-        red:{}
-    };
-    var units = Array.prototype.slice.call(document.getElementsByClassName('units'), 0);
+const removeDivByClass=(_className)=>{
+    var units = Array.prototype.slice.call(document.getElementsByClassName(_className), 0);
     for(let i = 0; i < units.length; i++){units[i].remove();};
-    init();
 }
+
+//이동가능한 지역 포인트 
+const makeAccessPoint=(x,y)=>{
+    let WIDTH = 50;
+    let HEIGHT = 50;
+    let pointDiv = document.createElement('div');
+    pointDiv.className = 'apoint';
+    pointDiv.style.position = 'absolute';
+    pointDiv.style.background = 'red';
+    pointDiv.style.width = WIDTH+'px';
+    pointDiv.style.height = HEIGHT+'px';
+    pointDiv.style.top = (y-(HEIGHT/2))+'px';
+    pointDiv.style.left = (x-(WIDTH/2))+'px';
+    document.getElementById('content').appendChild(pointDiv);
+}
+
+
+
+
---
+++ js/units.js
@@ -0,0 +1,224 @@
+
+/*
+    유닛 객체
+*/
+
+class JANG{
+    constructor(){
+        this.name = 'JANG';
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 80;
+        this.height = 80;
+        this.color;
+    }
+    
+    setUnit(team){
+        this.team = team;
+        if(team === 'blue'){this.init_position = [400,110];}
+        else if(team === 'red'){this.init_position = [400,770];}
+
+        this.current = this.init_position;
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
+
+
+class CHA{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 60;
+        this.height = 60;
+        this.color;
+    }
+    
+    setUnit(team,up){
+        this.name = 'CHA'+up;
+        this.team = team;
+        if(team === 'blue' && up === 'L'){this.init_position = [0,0];}
+        else if(team === 'blue' && up === 'R'){this.init_position = [800,0];}
+        else if(team === 'red' && up === 'L'){this.init_position = [0,880];}
+        else if(team === 'red' && up === 'R'){this.init_position = [800,880];}
+
+        this.current = this.init_position;
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
+
+
+class PO{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 60;
+        this.height = 60;
+        this.color;
+        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+            
+    setUnit(team,up){
+        this.name = 'PO'+up;
+        this.team = team;
+        if(team === 'blue' && up === 'L'){this.init_position = [100,220];}
+        else if(team === 'blue' && up === 'R'){this.init_position = [700,220];}
+        else if(team === 'red' && up === 'L'){this.init_position = [100,660];}
+        else if(team === 'red' && up === 'R'){this.init_position = [700,660];}
+
+        this.current = this.init_position;
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
+
+
+class JOL{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 45;
+        this.height = 45;
+        this.color;
+
+        //MOVE
+        this.DIRECTION = [];
+    }
+            
+    setUnit(team,up){
+        this.name = 'JOL'+up;
+        this.team = team;
+        if(team === 'blue' && up === '1'){this.init_position = [0,330];}
+        else if(team === 'blue' && up === '2'){this.init_position = [200,330];}
+        else if(team === 'blue' && up === '3'){this.init_position = [400,330];}
+        else if(team === 'blue' && up === '4'){this.init_position = [600,330];}
+        else if(team === 'blue' && up === '5'){this.init_position = [800,330];}
+        else if(team === 'red' && up === '1'){this.init_position = [0,550];}
+        else if(team === 'red' && up === '2'){this.init_position = [200,550];}
+        else if(team === 'red' && up === '3'){this.init_position = [400,550];}
+        else if(team === 'red' && up === '4'){this.init_position = [600,550];}
+        else if(team === 'red' && up === '5'){this.init_position = [800,550];}
+        
+        this.team === 'blue' ? this.DIRECTION.push([0,1]) : this.DIRECTION.push([0,-1]);
+        this.DIRECTION.push([0,0]);//뒤로는 못감
+        this.DIRECTION.push([-1,0]);
+        this.DIRECTION.push([+1,0]);
+        
+        this.current = this.init_position;
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+
+}
+
+
+class MA{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 60;
+        this.height = 60;
+        this.color;
+        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+            
+    setUnit(team,up,up2){
+        this.name = 'MA'+up;
+        this.team = team;
+        if(team === 'blue' && up === 'L' && up2 === 'L'){this.init_position = [100,0];}
+        else if(team === 'blue' && up === 'L' && up2 === 'R'){this.init_position = [200,0];}
+        else if(team === 'blue' && up === 'R' && up2 === 'L'){this.init_position = [600,0];}
+        else if(team === 'blue' && up === 'R' && up2 === 'R'){this.init_position = [700,0];}
+        else if(team === 'red' && up === 'L' && up2 === 'L'){this.init_position = [100,880];}
+        else if(team === 'red' && up === 'L' && up2 === 'R'){this.init_position = [200,880];}
+        else if(team === 'red' && up === 'R' && up2 === 'L'){this.init_position = [600,880];}
+        else if(team === 'red' && up === 'R' && up2 === 'R'){this.init_position = [700,880];}
+        
+        this.current = this.init_position;
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
+
+
+class SANG{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 60;
+        this.height = 60;
+        this.color;
+        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+            
+    setUnit(team,up,up2){
+        this.name = 'SANG'+up;
+        this.team = team;
+        if(team === 'blue' && up === 'L' && up2 === 'L'){this.init_position = [100,0];}
+        else if(team === 'blue' && up === 'L' && up2 === 'R'){this.init_position = [200,0];}
+        else if(team === 'blue' && up === 'R' && up2 === 'L'){this.init_position = [600,0];}
+        else if(team === 'blue' && up === 'R' && up2 === 'R'){this.init_position = [700,0];}
+        else if(team === 'red' && up === 'L' && up2 === 'L'){this.init_position = [100,880];}
+        else if(team === 'red' && up === 'L' && up2 === 'R'){this.init_position = [200,880];}
+        else if(team === 'red' && up === 'R' && up2 === 'L'){this.init_position = [600,880];}
+        else if(team === 'red' && up === 'R' && up2 === 'R'){this.init_position = [700,880];}
+        
+        this.current = this.init_position;
+        let unitDiv = document.createElement('div');
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
+
+class SA{
+    constructor(){
+        this.name;
+        this.selected = false;
+        this.team;
+        this.init_position;
+        this.current;//현재위치
+        this.prev;//이전위치
+        this.width = 45;
+        this.height = 45;
+        this.color;
+        // this.team=='blue'?this.color = 'rgba(0,0,255,1)':this.color = 'rgba(255,0,0,1)';
+    }
+            
+    setUnit(team,up){
+        this.name = 'SA'+up;
+        this.team = team;
+        if(team === 'blue' && up === 'L'){this.init_position = [300,0];}
+        else if(team === 'blue' && up === 'R'){this.init_position = [500,0];}
+        else if(team === 'red' && up === 'L'){this.init_position = [300,880];}
+        else if(team === 'red' && up === 'R'){this.init_position = [500,880];}
+        
+        this.current = this.init_position;
+        let unitDiv = document.createElement('div');
+        document.getElementById('content').appendChild(set_style(this));
+        UNITS[this.team][this.name] = this;
+    }
+}
